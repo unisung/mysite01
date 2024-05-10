a9e5324667c32f18101b7e74fb0bd95574683f62
unisung <vctor@naver.com>
2024-05-10 11:22:53 +0900

    first second.

--- pybo/urls.py
+++ pybo/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='details'),
     #http://127.0.0.1:8000/pybo/answer/create/1/
     path('answer/create/<int:question_id>/',
-          views.answer_create, name="answer_create")
+          views.answer_create, name="answer_create"),
+    path('question/create/', views.question_create,
+           name="question_create")
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -3,12 +3,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm
+from django.core.paginator import Paginator
 
 def home(request):
     # question_list = Question.objects.all()
     # order_by('속성명'), order_by('-속성명')
+    page = request.GET.get('page','1') #페이지
+    print('page:',page, 'type:',type(page))
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    #
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    #context = {'question_list': question_list}
     return render(request,
                   "pybo/question_list.html", context)
 
@@ -23,3 +31,20 @@ def answer_create(request, question_id):
     question.answer_set.create(content=request.POST.get('content'),
                                create_date=timezone.now())
     return redirect('pybo:details', question_id=question.id)
+
+
+def question_create(request):
+    if request.method=='POST':
+        form=QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False) #임시저장
+            question.create_date = timezone.now()
+            question.save() #db에 저장
+            return redirect('pybo:home')
+
+    else:# 'GET'
+        form = QuestionForm()
+        context = {'form': form}
+        return render(request,
+                      'pybo/question_form.html',
+                      context)
