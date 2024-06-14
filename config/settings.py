c8fc62749e3854e3b784c47267e4c2168d936f0c
길동이네 <vctor@naver.com>
2024-06-14 10:13:48 +0900

    aaa

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-19s75rdo78ysougv(39==5(geamfr^ja9sz)4j7fhu$ho0+z95
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
